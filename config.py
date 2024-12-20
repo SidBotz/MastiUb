4a42535e114b89e2ae7f7836778e61a2d2362e77
SidBotz <sidop148@gmail.com>
2024-12-20 22:33:33 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+API_ID = 5461760
+API_HASH = "396b10bcf5e1ed5fcc71f1603800b7cf"
+BOT_TOKEN = "7277830058:AAGhe3MH-kHxS_M82DZICfupJ7ncuLNgVa0"
+MONGO_URI = "mongodb://localhost:27017"
+DATABASE_NAME = "bot_database"
+LANGUAGES = ["eng", "hin", "fre", "tam", "per", "rus", "vie", "ind"]
+
